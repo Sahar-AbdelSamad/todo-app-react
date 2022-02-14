2478327b57beb8b3402f5110742ca8c5b23b4e95
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-02-14 23:53:36 +0200

    Create todo-list components

---
+++ src/components/TodoItem.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+class TodoItem extends React.Component {
+  render() {
+    return (
+    <li>
+      <input type="checkbox" checked={this.props.todo.completed} onChange={() => this.props.handleChangeProps(this.props.todo.id)} />{this.props.todo.title}
+      <button onClick={()=> this.props.deleteTodoProps(this.props.todo.id)}>Delete</button>
+    </li>
+    )
+  }
+}
+
+export default TodoItem
