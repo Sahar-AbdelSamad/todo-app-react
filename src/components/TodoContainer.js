d1810cfc1db0c5975c2ffac225bfff4f5991619a
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-02-15 00:50:20 +0200

    Edit To dos items

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -5,73 +5,84 @@ import InputTodo from './InputTodo';
 import TodosList from './TodosList';
 
 class TodoContainer extends React.Component {
-    state = {
+  state = {
+    todos: [
+      {
+        id: uuidv4(),
+        title: 'Setup development environment',
+        completed: true,
+      },
+      {
+        id: uuidv4(),
+        title: 'Develop website and add content',
+        completed: false,
+      },
+      {
+        id: uuidv4(),
+        title: 'Deploy to live server',
+        completed: false,
+      },
+    ],
+  };
+
+  handleChange = (id) => {
+    this.setState((prevState) => ({
+      todos: prevState.todos.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      }),
+    }));
+  };
+
+  delTodo = (id) => {
+    this.setState({
       todos: [
-        {
-          id: uuidv4(),
-          title: 'Setup development environment',
-          completed: true,
-        },
-        {
-          id: uuidv4(),
-          title: 'Develop website and add content',
-          completed: false,
-        },
-        {
-          id: uuidv4(),
-          title: 'Deploy to live server',
-          completed: false,
-        },
+        ...this.state.todos.filter((todo) => todo.id !== id),
       ],
+    });
+  }
+
+  addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title,
+      completed: false,
     };
+    this.setState({
+      todos: [...this.state.todos, newTodo],
+    });
+  };
+  setUpdate = (updatedTitle, id) => {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id === id) {
+          todo.title = updatedTitle
+        }
+        return todo
+      }),
+    })
+  }
 
-       handleChange = (id) => {
-         this.setState((prevState) => ({
-           todos: prevState.todos.map((todo) => {
-             if (todo.id === id) {
-               return {
-                 ...todo,
-                 completed: !todo.completed,
-               };
-             }
-             return todo;
-           }),
-         }));
-       };
-
-      delTodo = (id) => {
-        this.setState({
-          todos: [
-            ...this.state.todos.filter((todo) => todo.id !== id),
-          ],
-        });
-      }
-
-      addTodoItem = (title) => {
-        const newTodo = {
-          id: uuidv4(),
-          title,
-          completed: false,
-        };
-        this.setState({
-          todos: [...this.state.todos, newTodo],
-        });
-      };
-
-      render() {
-        return (
-            <div className="container">
-            <div className="inner">
-              <Header />
-              <InputTodo addTodoProps={this.addTodoItem} />
-              <TodosList
-                todos={this.state.todos}
-                handleChangeProps={this.handleChange}
-                deleteTodoProps={this.delTodo}
-              />
-            </div>
-          </div>
-        );
-      }
+  render() {
+    return (
+      <div className="container">
+        <div className="inner">
+          <Header />
+          <InputTodo addTodoProps={this.addTodoItem} />
+          <TodosList
+            todos={this.state.todos}
+            handleChangeProps={this.handleChange}
+            deleteTodoProps={this.delTodo}
+            setUpdate={this.setUpdate}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 export default TodoContainer;
--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -6,7 +6,13 @@ class TodosList extends React.Component {
     return (
       <ul>
         {this.props.todos.map((todo) => (
-          <TodoItem key={todo.id} todo={todo} handleChangeProps={this.props.handleChangeProps} deleteTodoProps={this.props.deleteTodoProps} />
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            handleChangeProps={this.props.handleChangeProps}
+            deleteTodoProps={this.props.deleteTodoProps}
+            setUpdate={this.props.setUpdate}
+          />
         ))}
       </ul>
     );
