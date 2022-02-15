2adce5bbdafa444c74f365c86a2f09efb81862f1
Sahar Abdel Samad <sahar.abdelsamad@gmail.com>
2022-02-15 18:23:18 +0200

    Add lifecycle methods

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -6,24 +6,8 @@ import TodosList from './TodosList';
 
 class TodoContainer extends React.Component {
   state = {
-    todos: [
-      {
-        id: uuidv4(),
-        title: 'Setup development environment',
-        completed: true,
-      },
-      {
-        id: uuidv4(),
-        title: 'Develop website and add content',
-        completed: false,
-      },
-      {
-        id: uuidv4(),
-        title: 'Deploy to live server',
-        completed: false,
-      },
-    ],
-  };
+    todos: [],
+  }
 
   handleChange = (id) => {
     this.setState((prevState) => ({
@@ -66,7 +50,22 @@ class TodoContainer extends React.Component {
         return todo
       }),
     })
-  }
+  };
+  componentDidUpdate(prevProps, prevState) {
+    if(prevState.todos !== this.state.todos) {
+      const temp = JSON.stringify(this.state.todos)
+      localStorage.setItem("todos", temp)
+    }
+  };
+  componentDidMount() {
+    const temp = localStorage.getItem("todos")
+    const loadedTodos = JSON.parse(temp)
+    if (loadedTodos) {
+      this.setState({
+        todos: loadedTodos
+      })
+    }
+  };
 
   render() {
     return (
